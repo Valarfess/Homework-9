cfc500b2f926423ef66c6a47570955d42d3f294f
valarfess <valarfess@gmail.com>
2021-02-21 20:40:29 +0300

    Домашка 9

--- src/academy/belhard/Person.java
+++ src/academy/belhard/Person.java
@@ -52,18 +52,4 @@ public class Person {
                     ", Location info: " + address.getCity() + ", ul." + address.getStreet() + ", #" + address.getHouseNumber() +
                     '.';
     }
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (!(o instanceof Person)) return false;
-//            Person person = (Person) o;
-//            return  firstName.equals(person.firstName) &&
-//                    lastName.equals(person.lastName) &&
-//                    address.equals(person.address);
-//        }
-//
-//        @Override
-//        public int hashCode() {
-//            return Objects.hash(firstName, lastName, address);
-//        }
-}
+
